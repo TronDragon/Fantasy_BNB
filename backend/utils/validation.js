2582bafab8cf755067926a35545525c505e9abc6
Brandon Allen <brandon_allen93@yahoo.com>
2024-02-16 00:12:12 -0500

    testing

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
-const handleValidationErrors = (req, _res, next) => {
+const handleValidationErrors = (req, _, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
